210b22c6fe0358a1783f3dad146014861e3b4dfc
Ethan-Shek <dinoeshek@gmail.com>
2024-10-03 08:22:56 -0400

    Part one intro

---
+++ Participation_Logic_and_Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Participation_Logic_and_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = "adventurer";
+
+            Console.WriteLine("Welcome to te jungle \n We got fun and games \nWe got everything you want \nHoney, we know the names \n");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
+
+
+
+
+
+        }
+    }
+}
